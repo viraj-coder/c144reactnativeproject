7a4607525a358f3ab201b164e2725d499e0e50e7
Viraj Mehta <54108753+viraj-coder@users.noreply.github.com>
2020-12-23 13:16:18 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,122 +1,117 @@
-import React from "react"
-import {Text,View,TouchableOpacity,Image} from react-native
-export default class Home extends React.Component{
-    constructor(){
-        super()
-        this.state={articledetails:{}}
-    }   
-    getarticle=()=>{
-    const url="http://127.0.0.1/5000/get-movie"//use api from ngork
-    axios.get(url).then(responce=>{let details=responce.data.data
-    details["duration"]=this.timeconvert(details.duration)
-    this.setState({articledetails:details})
-          })
-    .catch(error=>{console.log(error.message)})
-    }
-componentDidMount(){this.getarticle()}
-likedarticle=()=>{const url="http://127.0.0.1/5000/liked-article"
-axios.post(url).then(responce=>{this.getmovie()})
-.catch(error=>{console.log(error.message)})}
+import React, { Component } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Header, Icon } from "react-native-elements";
+import WebView from "react-native-webview";
+import axios from "axios";
 
-unlikedarticle=()=>{
-const url="http://127.0.0.1/5000/unliked-article"
-axios.post(url).then(responce=>{this.getmovie()})
-.catch(error=>{console.log(error.message)})
-}
-didnotseearticle=()=>{
-const url="http://127.0.0.1/5000/get-movie"
-axios.post(url).then(responce=>{this.getarticle()})
-.catch(error=>{console.log(error.message)})
+export default class HomeScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articleDetails: {}
+    };
+  }
 
-}
-timeconvert(num){
-    var hours=Math.float(num/60)
-    var minutes=num%60
-    return `${hours} hrs ${minutes} mins`
-}
-render(){
-    const {articledetails}=this.state
-    if (articledetails.poster_link){
-        const{poster_link,title,release_data,duration,overview,rating}=moviedetails
-        return(
-            <View style={styles.container}>
-                <View style={styles.headercontainer}>
-                <Header centerComponent={{text:"Article Recommended",style=styles.headertitle}}
-                rightComponent={{icon:"search",color:"#fff"}}
-                backgroundcolor={"#df009"}
-                containerStyle={{flex:1}}/>
-                </View>
-                <View>
-                    <Image source={{uri:poster_link}}>
+  componentDidMount() {
+    this.getArticle();
+  }
 
-                    </Image>
-                </View>
-                <View>
-                    <Text>
-                        {title}
-                    </Text>
-                    <Text>
-                        {`${release_date.split('-')[0]}`} | ${duration}
-                    </Text>
-                </View>
-                <View>
-                    <AirbnbRating
-                    count={10}
-                    reviews={['','','','','']}
-                    defaultRating={rating}
-                    isDisabled={True}
-                    size={RFValue(25)}
-                    >
+  timeConvert(num) {
+    var hours = Math.floor(num / 60);
+    var minutes = num % 60;
+    return `${hours} hrs ${minutes} mins`;
+  }
 
-                    </AirbnbRating>
-                </View>
-                <View>
-                    <Text>
-                    {overview}
-                    </Text>
-                </View>
-                <View>
-                    <TouchableOpacity
-                    onPress={this.likedarticle}
-                    >
-                        <Icon
-                        reverse
-                        name={'check'}
-                        type={'entypo'}
-                        size={RFValue(30)}
-                        color={'green'}
-                        >
+  getArticle = () => {
+    const url = "http://localhost:5000/get-article";
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({ articleDetails: response.data.data });
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
 
-                        </Icon>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                    onPress={this.unlikedmovie}
-                    >
-                        <Icon
-                        reverse
-                        name={'cross'}
-                        type={'entypo'}
-                        size={RFValue(30)}
-                        color={'red'}
-                        >
+  likedArticle = () => {
+    const url = "http://localhost:5000/liked-article";
+    axios
+      .post(url)
+      .then(response => {
+        this.getArticle();
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
 
-                        </Icon>
-                    </TouchableOpacity>
-                    <View>
-                        <TouchableOpacity
-                            onPress={this.didnotseearticle}
-                            >
-                                <Text>
-                                    Did Not Watch
-                                </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                </View>
+  unlikedArticle = () => {
+    const url = "http://localhost:5000/unliked-article";
+    axios
+      .post(url)
+      .then(response => {
+        this.getArticle();
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
+
+  render() {
+    const { articleDetails } = this.state;
+    if (articleDetails.url) {
+      const { url } = articleDetails;
+
+      return (
+        <View>
+          <View>
+            <Header
+              centerComponent={{
+                text: "Recommended",
                 
-
-        )
+              }}
+              rightComponent={{
+                icon: "search",
+                color: "#fff",
+                onPress: () =>
+                  this.props.navigation.navigate("RecommendedArticles")
+              }}
+              backgroundColor={"#d500f9"}
+              containerStyle={{ flex: 1 }}
+            />
+          </View>
+          <View >
+            <WebView source={{ uri: url }} />
+          </View>
+          <View>
+            <View>
+              <TouchableOpacity onPress={this.likedArticle}>
+                <Icon
+                  reverse
+                  name={"check"}
+                  type={"entypo"}
+                  size={30}
+                  color={"#76ff03"}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.unlikedArticle}>
+                <Icon
+                  reverse
+                  name={"cross"}
+                  type={"entypo"}
+                  size={30}
+                  color={"#ff1744"}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      );
     }
+    return null;
+  }
 }
-}     
-    
+
+
+
